0ac45c5a23261408939f57a04da0a1643cfc80f0
sa212320 <sa212320@gmail.com>
2021-07-28 14:03:06 +0800

    JSharefileprovider 版本

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { registerPlugin } from '@capacitor/core';
+
+import type { SharePlugin } from './definitions';
+
+const Share = registerPlugin<SharePlugin>('Share', {
+  web: () => import('./web').then(m => new m.ShareWeb()),
+});
+
+export * from './definitions';
+export { Share };
